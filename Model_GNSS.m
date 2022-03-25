d7b84a31000e54be00e1ddd4c636b5010eadfd4b
Kana Nagai <knagai@gmail.com>
2022-03-25 08:30:05 -0500

    ver.4

--- Model_GNSS.m
+++ Model_GNSS.m
@@ -1,6 +1,7 @@
 % GNSS EKF measurement
 % Written by: Kana Nagai 2022/3/20
 
+
 %% Setup
 clc;clear;beep off;close all
 addpath('../Models/constell');
@@ -8,9 +9,11 @@ addpath('../Models/function');
 set(0, 'DefaultLineLineWidth', 2);
 set(0,'defaultAxesFontSize',15);
 
+
 %% Input
 getinput
 IMUType = 'STIM300';
+
 
 %%  GNSS Initialization with the EKF
 
@@ -23,7 +26,9 @@ IMUType = 'STIM300';
 % get alignmnet
 [Pbar, save1] = getGNSS_Ali(Pbar,vis_alm,time_index,IMUType);
 
+
 %% Plot *******************************************************************
+
 figure
 save_all = [save0;save1];
 plot(1:length(save_all),save_all);
